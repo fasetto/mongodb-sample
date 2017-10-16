010d7c701c59b97f8a906cbcccad21991e80eb73
SERKAN BIRCAN <serkan@siniradam.net>
2017-10-16 17:40:37 +0300

    Added IRepository interface.

---
+++ MongoDB_Sample/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MongoDB_Sample
+{
+    public interface IRepository<TEntity> where TEntity : EntityBase
+    {
+        Task AddNewAsync(TEntity entity);
+        Task<bool> UpdateAsync(ObjectId id, string field, string value);
+        Task<TEntity> GetByIdAsync(ObjectId id);
+        Task<IEnumerable<TEntity>> GetByFieldAsync(string field, string value);
+        Task<long> DeleteAllAsync();
+    }
+}
