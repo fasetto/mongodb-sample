24bcd7d6858aba9561c2260c98b80a16cb665d02
SERKAN BIRCAN <serkan@siniradam.net>
2017-10-16 17:39:56 +0300

    Added User entity.

---
+++ MongoDB_Sample/User.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB_Sample
+{
+    public class User : EntityBase
+    {
+        [BsonElement("firstname")]
+        public string FirstName { get; set; }
+
+        [BsonElement("lastname")]
+        public string LastName { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+    }
+}
