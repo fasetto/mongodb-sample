92dfb24f2cf5f3db528bfaa7237c5fd88c3755a3
SERKAN BIRCAN <serkan@siniradam.net>
2017-10-16 17:41:33 +0300

    Added UserRepository.

---
+++ MongoDB_Sample/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDB_Sample
+{
+    public class UserRepository : MongoRepository<User>
+    {
+        private readonly MongoDataContext _dataContext;
+        protected override IMongoCollection<User> Collection => _dataContext.Database.GetCollection<User>("users");
+
+        public UserRepository(MongoDataContext dataContext)
+            : base(dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
