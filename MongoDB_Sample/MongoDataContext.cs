aed30426eaf7a69e0bff295a02773cc5de0d0f05
SERKAN BIRCAN <serkan@siniradam.net>
2017-10-16 17:40:57 +0300

    Added MongoDataContext.

---
+++ MongoDB_Sample/MongoDataContext.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDB_Sample
+{
+    public class MongoDataContext
+    {
+        public IMongoDatabase Database { get; }
+
+        public MongoDataContext(string connectionString)
+        {
+            var mongoUrl = new MongoUrl(connectionString);
+
+            IMongoClient client = new MongoClient(mongoUrl);
+            Database = client.GetDatabase(mongoUrl.DatabaseName);
+        }
+    }
+}
