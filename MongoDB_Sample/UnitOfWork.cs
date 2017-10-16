3de9028bae1619fbb3959283e336cace976fa62c
SERKAN BIRCAN <serkan@siniradam.net>
2017-10-16 17:41:51 +0300

    Added UnitOfWork.

---
+++ MongoDB_Sample/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿namespace MongoDB_Sample
+{
+    public class UnitOfWork
+    {
+        private readonly MongoDataContext _dataContext;
+
+        public UserRepository Users { get; private set; }
+
+        public UnitOfWork(MongoDataContext dataContext)
+        {
+            _dataContext = dataContext;
+
+            Users = new UserRepository(dataContext);
+        }
+
+        public bool CheckConnection()
+        {
+            try
+            {
+                _dataContext.Database.ListCollections();
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
