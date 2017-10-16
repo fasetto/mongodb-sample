603b12f3b2059f158d8282750e28c243523e1e26
SERKAN BIRCAN <serkan@siniradam.net>
2017-10-16 17:39:31 +0300

    Added EntityBase class.

---
+++ MongoDB_Sample/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB_Sample
+{
+    public abstract class EntityBase
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+    }
+}
